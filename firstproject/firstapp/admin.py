5dc617cef3d9ff5d3d5a51ed12a83ac20863b803
macbook <macbook@macbooks-MacBook-Pro.local>
2020-06-14 12:34:31 -0500

    Task Updated

---
+++ firstproject/firstapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Bootcamp_Candidate
+# Register your models here.
+admin.site.register(Bootcamp_Candidate)
---
+++ firstproject/firstapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Bootcamp_Candidate(models.Model):
+    First_name = models.CharField(max_length=30)
+    Last_name = models.CharField(max_length=30)
+    Address = models.CharField(max_length=30)
